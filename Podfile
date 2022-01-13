cdd81a3c9056e70d24acdeed8dde273acc979c64
zafyraminx <zafra_angelo_26@yahoo.com.ph>
2022-01-13 13:47:39 +0800

    - Added Firestore - Added Registration function

---
+++ Podfile
@@ -0,0 +1,27 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RestfulLogin' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RestfulLogin
+  # RestAPI
+  pod 'Alamofire'
+  pod 'AlamofireImage'
+  
+  # Firebase
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+
+  target 'RestfulLoginTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RestfulLoginUITests' do
+    # Pods for testing
+  end
+
+end
--- RestfulLogin/Controllers/RegisterViewController.swift
+++ RestfulLogin/Controllers/RegisterViewController.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import UIKit
-import Alamofire
+import FirebaseFirestore
 
 class RegisterViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
     
@@ -48,38 +48,23 @@ class RegisterViewController: UIViewController, UITextViewDelegate, UITextFieldD
             return
         }
         
-        let parameters: [String: Any] = [
-            "name" : nameTextField.text ?? "",
-            "email" : emailTextField.text ?? "",
-            "password" : passwordTextField.text ?? ""
-        ]
-        AF.request(
-            "https://reqres.in/api/users",
-            method: .post,
-            parameters: parameters)
-            .responseString(completionHandler: { response in
-                switch response.result {
-                case .success(let value):
-                    //end loading..
-                    print("value**: \(value)")
-                    do {
-                        let data = try value.data(using: .utf8)!
-                        print("data \(data)")
-                        let decoder = JSONDecoder()
-                        if let responseListData = try? decoder.decode(RegisterResponse.self, from: data) {
-                            print("responseListData \(responseListData)")
-                        }
-                    } catch {
-                        // handle error
-                        print("error")
-                    }
-                case .failure(let error):
-                    //end loading..
-                    print(error)
-                    let alert = UIAlertController(title: "Error", message: error.errorDescription, preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                    self.present(alert, animated: true, completion: nil)
-                }
-            })
+        let db = Firestore.firestore()
+        var ref: DocumentReference? = nil
+        ref = db.collection("users").addDocument(data: [
+            "name": nameTextField.text ?? "",
+            "email": emailTextField.text ?? "",
+            "password": confirmTextfield.text ?? ""
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+                let alert = UIAlertController(title: "Registration", message: "Registration Successful", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { _ in 
+                    self.dismiss(animated: true)
+                }))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
     }
 }
