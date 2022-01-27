f58eb4db149abfcbd83c7ccb520ded9fb07f38b1
zafyraminx <80800798+michael-zafra-da5@users.noreply.github.com>
2022-01-27 13:17:21 +0800

    - Implemented simplified conditional statement

--- RestfulLogin/Controllers/SplashVC.swift
+++ RestfulLogin/Controllers/SplashVC.swift
@@ -37,26 +37,56 @@ class SplashVC: UIViewController {
     }
     
     private func initTimer() {
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-            self.timeLeft -= 1
-            print("Time Left \(self.timeLeft)")
-            if(self.timeLeft==0){
-                timer.invalidate()
-                
-                let userID = UserDefaults.standard.string(forKey: Constants().userID)
-                print("😅userID \(userID ?? "")😅")
-                if userID != nil {
-                    let storyboard = UIStoryboard(name: "Home", bundle: nil)
-                    let detailVC = storyboard.instantiateViewController(withIdentifier: "homeSB") as! HomeViewController
-                    detailVC.modalPresentationStyle = .fullScreen
-                    self.present(detailVC, animated: true, completion: nil)
-                } else {
-                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                    let detailVC = storyboard.instantiateViewController(withIdentifier: "loginSB") as! ViewController
-                    detailVC.modalPresentationStyle = .fullScreen
-                    self.present(detailVC, animated: true, completion: nil)
-                }
-            }
+        // For Countdown
+        //        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+        //            self.timeLeft -= 1
+        //            print("Time Left \(self.timeLeft)")
+        //            if(self.timeLeft==0){
+        //                timer.invalidate()
+        //
+        //                let userID = UserDefaults.standard.string(forKey: Constants().userID)
+        //                print("😅userID \(userID ?? "")😅")
+        //                if userID != nil {
+        //                    let storyboard = UIStoryboard(name: "Home", bundle: nil)
+        //                    let detailVC = storyboard.instantiateViewController(withIdentifier: "homeSB") as! HomeViewController
+        //                    detailVC.modalPresentationStyle = .fullScreen
+        //                    self.present(detailVC, animated: true, completion: nil)
+        //                } else {
+        //                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        //                    let detailVC = storyboard.instantiateViewController(withIdentifier: "loginSB") as! ViewController
+        //                    detailVC.modalPresentationStyle = .fullScreen
+        //                    self.present(detailVC, animated: true, completion: nil)
+        //                }
+        //            }
+        //        }
+        
+        // For based on interval
+        print("Timer start \(Date())")
+        Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { timer in
+            print("Timer finish \(Date())")
+            timer.invalidate()
+            let userID = UserDefaults.standard.string(forKey: Constants().userID)
+            print("😅userID \(userID ?? "")😅")
+            
+            //normal conditional statement for opening page
+//            if userID != nil {
+//                let storyboard = UIStoryboard(name: "Home", bundle: nil)
+//                let detailVC = storyboard.instantiateViewController(withIdentifier: "homeSB") as! HomeViewController
+//                detailVC.modalPresentationStyle = .fullScreen
+//                self.present(detailVC, animated: true, completion: nil)
+//            } else {
+//                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//                let detailVC = storyboard.instantiateViewController(withIdentifier: "loginSB") as! ViewController
+//                detailVC.modalPresentationStyle = .fullScreen
+//                self.present(detailVC, animated: true, completion: nil)
+//            }
+            
+            //Simplified conditional statement for opening page
+            let storyboardName = (userID != nil ? "Home" : "Main")
+            let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+            let detailVC = storyboard.instantiateViewController(withIdentifier: storyboardName == "Main" ? "loginSB" : "homeSB")
+            detailVC.modalPresentationStyle = .fullScreen
+            self.present(storyboardName == "Main" ? (detailVC as! ViewController) : (detailVC as! HomeViewController), animated: true, completion: nil)
         }
     }
 }
